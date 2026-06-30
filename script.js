df3244b2102c0d1254d6495b005be1b740e3c66c
CattacoCattaco <cattacoiscool@gmail.com>
2026-06-30 15:39:26 -0700

    Add Better Styling
    Added better styling to the next and previous buttons, as well as disabling the previous button when there is no previous card to go back to

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const FLASHCARD = document.getElementById("flashcard");
 const FLASHCARD_TEXT = document.getElementById("flashcard-text");
+const PREV_BUTTON = document.getElementById("prev-button");
 
 let cards = [
     {
@@ -22,19 +23,24 @@ let termFace = true;
 
 function main() {
     nextCard();
+    PREV_BUTTON.disabled = true;
 }
 
 function prevCard() {
     if(index == 0) {
         return;
     }
-    
+
     index--;
 
     card = sequence[index];
     termFace = true;
 
     FLASHCARD_TEXT.innerText = card.term;
+
+    if(index == 0) {
+        PREV_BUTTON.disabled = true;
+    }
 }
 
 function nextCard() {
@@ -47,6 +53,8 @@ function nextCard() {
     termFace = true;
 
     FLASHCARD_TEXT.innerText = card.term;
+
+    PREV_BUTTON.disabled = false;
 }
 
 function flipCard() {
