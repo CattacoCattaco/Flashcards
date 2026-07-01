d0a70c9e62b040a2d6ba53691c145d51a092d506
CattacoCattaco <cattacoiscool@gmail.com>
2026-06-30 18:25:18 -0700

    Favor Unseen Cards
    Now cards that have already showed up only have a 10% chance of appearing. Once all cards have been seen, the cycle resets.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const FLASHCARD = document.getElementById("flashcard");
 const FLASHCARD_TEXT = document.getElementById("flashcard-text");
+const NEXT_BUTTON = document.getElementById("next-button");
 const PREV_BUTTON = document.getElementById("prev-button");
 
 let cards = [
@@ -21,6 +22,9 @@ let cards = [
     },
 ];
 
+let unusedCards = [];
+let reuseCardChance = 0.1;
+
 let sequence = [];
 
 window.onload = main;
@@ -30,6 +34,13 @@ let index = -1;
 let termFace = true;
 
 function main() {
+    reset();
+}
+
+function reset() {
+    unusedCards = cards.slice();
+    sequence = [];
+    index = -1;
     nextCard();
     PREV_BUTTON.disabled = true;
 }
@@ -52,12 +63,36 @@ function prevCard() {
 }
 
 function nextCard() {
+    if(unusedCards.length < 1) {
+        reset();
+        return;
+    }
+
     index++;
     if(index >= sequence.length) {
-        let newCard = cards[Math.floor(Math.random() * cards.length)];
-        while(card == newCard) {
+        let newCard;
+
+        if(Math.random() < reuseCardChance) {
+            console.log("Hi");
             newCard = cards[Math.floor(Math.random() * cards.length)];
+            while(card == newCard) {
+                newCard = cards[Math.floor(Math.random() * cards.length)];
+            }
+
+            if(unusedCards.includes(newCard)) {
+                let index = unusedCards.indexOf(newCard);
+                console.log(unusedCards)
+                console.log(index);
+                unusedCards.splice(index, 1);
+            }
         }
+        else {
+            console.log("B");
+            let newCardIndex = Math.floor(Math.random() * unusedCards.length);
+            newCard = unusedCards[newCardIndex];
+            unusedCards.splice(newCardIndex, 1);
+        }
+        
         sequence.push(newCard);
     }
 
